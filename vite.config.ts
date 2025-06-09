5e0453af48ee9ab4af52263d019c96e9b94abc06
khoaphunsonac <khoa.trandang020704@hcmut.edu.vn>
2025-06-09 09:02:26 +0700

    fix localhost bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ReactProject2/',
+  
   plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/ReactProject2/' : '/',
 })
